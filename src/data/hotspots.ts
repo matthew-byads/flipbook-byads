1e74ac4452aa1899529dfd1944c653a9a0b74e3b
matthew-byads <mateo@byads.co>
2026-03-30 17:40:38 -0500

    feat(FBK-007): hotspots updated

--- src/data/hotspots.ts
+++ src/data/hotspots.ts
@@ -9,73 +9,332 @@ export type Hotspot = {
 
 export const hotspots: Hotspot[] = [
     {
-        id: "admin-f3nt6tsna",
-        pageId: "026",
-        productId: "p1",
-        xPct: 87.33535967578521,
-        yPct: 26.98529988020466
+        "id": "admin-f3nt6tsna",
+        "pageId": "026",
+        "productId": "p1",
+        "xPct": 87.33535967578521,
+        "yPct": 26.98529988020466
     },
     {
-        id: "admin-70egvf9gn",
-        pageId: "016",
-        productId: "p3",
-        xPct: 83.08004052684905,
-        yPct: 43.322249465399295
+        "id": "admin-70egvf9gn",
+        "pageId": "016",
+        "productId": "p3",
+        "xPct": 83.08004052684905,
+        "yPct": 43.322249465399295
     },
     {
-        id: "admin-o5y1ek63v",
-        pageId: "016",
-        productId: "p1",
-        xPct: 22.69503546099291,
-        yPct: 52.63717686046642
+        "id": "admin-o5y1ek63v",
+        "pageId": "016",
+        "productId": "p1",
+        "xPct": 22.69503546099291,
+        "yPct": 52.63717686046642
     },
     {
-        id: "admin-dct363f6r",
-        pageId: "017",
-        productId: "p3",
-        xPct: 18.135764944275582,
-        yPct: 33.43409576909728
+        "id": "admin-dct363f6r",
+        "pageId": "017",
+        "productId": "p3",
+        "xPct": 18.135764944275582,
+        "yPct": 33.43409576909728
     },
     {
-        id: "admin-maer2m4qb",
-        pageId: "017",
-        productId: "p4",
-        xPct: 79.33130699088146,
-        yPct: 33.43409576909728
+        "id": "admin-maer2m4qb",
+        "pageId": "017",
+        "productId": "p4",
+        "xPct": 79.33130699088146,
+        "yPct": 33.43409576909728
     },
     {
-        id: "admin-4jy8119qe",
-        pageId: "017",
-        productId: "p5",
-        xPct: 45.49138804457954,
-        yPct: 32.28764316662748
+        "id": "admin-4jy8119qe",
+        "pageId": "017",
+        "productId": "p5",
+        "xPct": 45.49138804457954,
+        "yPct": 32.28764316662748
     },
     {
-        id: "admin-9cbtdcljn",
-        pageId: "018",
-        productId: "p3",
-        xPct: 16.0081053698075,
-        yPct: 31.57111029008386
+        "id": "admin-9cbtdcljn",
+        "pageId": "018",
+        "productId": "p3",
+        "xPct": 16.0081053698075,
+        "yPct": 31.57111029008386
     },
     {
-        id: "admin-3pbhvslrs",
-        pageId: "018",
-        productId: "p2",
-        xPct: 50.86119554204661,
-        yPct: 34.29393522094963
+        "id": "admin-3pbhvslrs",
+        "pageId": "018",
+        "productId": "p2",
+        "xPct": 50.86119554204661,
+        "yPct": 34.29393522094963
     },
     {
-        id: "admin-pquusf8qy",
-        pageId: "018",
-        productId: "p5",
-        xPct: 82.8774062816616,
-        yPct: 30.567964262922782
+        "id": "admin-pquusf8qy",
+        "pageId": "018",
+        "productId": "p5",
+        "xPct": 82.8774062816616,
+        "yPct": 30.567964262922782
     },
     {
-        id: "admin-dyfw63kc6",
-        pageId: "004",
-        productId: "p5",
-        xPct: 50.050658561296856,
-        yPct: 74.56308288270132
+        "id": "admin-6q1wml9ef",
+        "pageId": "062",
+        "productId": "p279",
+        "xPct": 43.90625,
+        "yPct": 47.29095084344838
+    },
+    {
+        "id": "admin-6k149m9cv",
+        "pageId": "005",
+        "productId": "p1",
+        "xPct": 24.68765068146168,
+        "yPct": 37.42883165205531
+    },
+    {
+        "id": "admin-8i4minqc1",
+        "pageId": "005",
+        "productId": "p2",
+        "xPct": 32.343947411788406,
+        "yPct": 37.76033910449823
+    },
+    {
+        "id": "admin-mop52jcxm",
+        "pageId": "005",
+        "productId": "p3",
+        "xPct": 40.3127460494754,
+        "yPct": 37.539334136202946
+    },
+    {
+        "id": "admin-562qucdpu",
+        "pageId": "005",
+        "productId": "p4",
+        "xPct": 49.06279945556308,
+        "yPct": 37.42883165205531
+    },
+    {
+        "id": "admin-cn55k93ly",
+        "pageId": "005",
+        "productId": "p5",
+        "xPct": 57.344100000610354,
+        "yPct": 37.539334136202946
+    },
+    {
+        "id": "admin-7ghius64v",
+        "pageId": "005",
+        "productId": "p6",
+        "xPct": 65.9379024530179,
+        "yPct": 36.76581674716945
+    },
+    {
+        "id": "admin-vf13h9c2b",
+        "pageId": "005",
+        "productId": "p7",
+        "xPct": 74.53170490542544,
+        "yPct": 37.870841588645874
+    },
+    {
+        "id": "admin-zztyje9bt",
+        "pageId": "005",
+        "productId": "p8",
+        "xPct": 24.06264686674113,
+        "yPct": 46.269030383866635
+    },
+    {
+        "id": "admin-jebf2fu38",
+        "pageId": "005",
+        "productId": "p9",
+        "xPct": 32.81270027282881,
+        "yPct": 46.49003535216192
+    },
+    {
+        "id": "admin-wydbgjt8y",
+        "pageId": "005",
+        "productId": "p10",
+        "xPct": 42.03150653995691,
+        "yPct": 45.053503058242576
+    },
+    {
+        "id": "admin-zqzbivr9j",
+        "pageId": "005",
+        "productId": "p11",
+        "xPct": 48.75029754820281,
+        "yPct": 46.15852789971899
+    },
+    {
+        "id": "admin-2kp73mr4h",
+        "pageId": "005",
+        "productId": "p12",
+        "xPct": 58.43785667637131,
+        "yPct": 44.61149312165201
+    },
+    {
+        "id": "admin-4in4cj6h3",
+        "pageId": "005",
+        "productId": "p13",
+        "xPct": 67.187910082459,
+        "yPct": 45.385010510685504
+    },
+    {
+        "id": "admin-ctn4c21s0",
+        "pageId": "005",
+        "productId": "p14",
+        "xPct": 74.37545395174531,
+        "yPct": 45.274508026537866
+    },
+    {
+        "id": "admin-168112lo2",
+        "pageId": "006",
+        "productId": "p57",
+        "xPct": 64.84353542196912,
+        "yPct": 43.064458343585024
+    },
+    {
+        "id": "admin-yu0re5rb2",
+        "pageId": "007",
+        "productId": "p57",
+        "xPct": 30.62518692130689,
+        "yPct": 31.461697508082654
+    },
+    {
+        "id": "admin-owmoa0pjx",
+        "pageId": "007",
+        "productId": "p58",
+        "xPct": 52.031567575485695,
+        "yPct": 28.699135404391612
+    },
+    {
+        "id": "admin-in9lixb5i",
+        "pageId": "007",
+        "productId": "p59",
+        "xPct": 76.40671634958709,
+        "yPct": 27.262603110472266
+    },
+    {
+        "id": "admin-uqn6zksxp",
+        "pageId": "007",
+        "productId": "p62",
+        "xPct": 50.00030517764391,
+        "yPct": 55.9932489888591
+    },
+    {
+        "id": "admin-52khx9kh9",
+        "pageId": "007",
+        "productId": "p63",
+        "xPct": 72.50044250758366,
+        "yPct": 54.888224147382694
+    },
+    {
+        "id": "admin-8ynylumh2",
+        "pageId": "007",
+        "productId": "p65",
+        "xPct": 31.56269264338772,
+        "yPct": 80.6353029537832
+    },
+    {
+        "id": "admin-51imblzdk",
+        "pageId": "007",
+        "productId": "p66",
+        "xPct": 48.75029754820281,
+        "yPct": 77.87274085009216
+    },
+    {
+        "id": "admin-tzg79uwgh",
+        "pageId": "007",
+        "productId": "p67",
+        "xPct": 66.40665531405831,
+        "yPct": 74.66816880981055
+    },
+    {
+        "id": "admin-kfd1oajwt",
+        "pageId": "007",
+        "productId": "p61",
+        "xPct": 26.562662125623326,
+        "yPct": 55.55123905226854
+    },
+    {
+        "id": "admin-024fmuhot",
+        "pageId": "008",
+        "productId": "p60",
+        "xPct": 30.780827519699216,
+        "yPct": 28.699135404391612
+    },
+    {
+        "id": "admin-qbftx770z",
+        "pageId": "008",
+        "productId": "p68",
+        "xPct": 50.78094959075678,
+        "yPct": 26.931095658029342
+    },
+    {
+        "id": "admin-ombvskxx6",
+        "pageId": "008",
+        "productId": "p64",
+        "xPct": 30.62457656601908,
+        "yPct": 56.43525892544967
+    },
+    {
+        "id": "admin-w8xuykvbm",
+        "pageId": "008",
+        "productId": "p72",
+        "xPct": 47.4996795634739,
+        "yPct": 57.20877631448317
+    },
+    {
+        "id": "admin-f7ezps9ja",
+        "pageId": "008",
+        "productId": "p76",
+        "xPct": 72.96858501333627,
+        "yPct": 54.888224147382694
+    },
+    {
+        "id": "admin-mcldsy9si",
+        "pageId": "008",
+        "productId": "p79",
+        "xPct": 53.43721580331911,
+        "yPct": 78.97776569156856
+    },
+    {
+        "id": "admin-3liqoxk8r",
+        "pageId": "008",
+        "productId": "p80",
+        "xPct": 69.21856212501297,
+        "yPct": 78.42525327083037
+    },
+    {
+        "id": "admin-clvu8816m",
+        "pageId": "009",
+        "productId": "p69",
+        "xPct": 30.937688828667163,
+        "yPct": 25.715568332405287
+    },
+    {
+        "id": "admin-yhd8dr1f9",
+        "pageId": "009",
+        "productId": "p70",
+        "xPct": 48.43779564084254,
+        "yPct": 25.826070816552928
+    },
+    {
+        "id": "admin-ljkww2g64",
+        "pageId": "009",
+        "productId": "p71",
+        "xPct": 69.53167438766106,
+        "yPct": 25.826070816552928
+    },
+    {
+        "id": "admin-i5i51bu3z",
+        "pageId": "009",
+        "productId": "p74",
+        "xPct": 49.06279945556308,
+        "yPct": 55.9932489888591
+    },
+    {
+        "id": "admin-xue15cgx3",
+        "pageId": "009",
+        "productId": "p77",
+        "xPct": 34.53146076331032,
+        "yPct": 81.96133276355489
+    },
+    {
+        "id": "admin-yw410fmxw",
+        "pageId": "009",
+        "productId": "p78",
+        "xPct": 50.31280708500419,
+        "yPct": 82.40334270014546
     }
 ];
